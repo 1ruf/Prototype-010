828a24e85b72bd2e1371e3423be35cb9c4423f49
고한결 <gyeol0411@naver.com>
2025-07-06 23:44:13 +0900

    .update bullet

--- Prototype-010/Assets/02.Scripts/Bullet.cs
+++ Prototype-010/Assets/02.Scripts/Bullet.cs
@@ -1,8 +1,10 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed = 100;
+    [SerializeField] private float recochetAngle = 85f;
     private Rigidbody _rigid;
 
     private void Awake()
@@ -18,6 +20,30 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         print("HIT:" + collision.gameObject.name);
+        
         //Destroy(gameObject);
+    }
+
+    private void CheckRecochet(Collision target)
+    {
+        ContactPoint cP = target.contacts[0];
+        Vector3 normal = cP.normal;
+        Vector3 contactVelocity = target.relativeVelocity;
+
+        float angle = Vector3.Angle(-contactVelocity, normal);
+
+        if(CalcRecochet(angle))
+        {
+            print("recochet");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }    
+
+    private bool CalcRecochet(float angle)
+    {
+        return angle >= recochetAngle;
     }
 }
